c9d38f0e13886e78270d8f5c6dfe29b39dc22f08
Leniiaa <129392024+Leniiaa@users.noreply.github.com>
2025-01-13 11:17:02 +0100

    test

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import * as React from "react"
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 
 const rows: GridRowsProp = [
-  { id: 1, col1: 'Pipi', col2: 'er lav' },
-  { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-  { id: 3, col1: 'Sjokolade', col2: 'is Amazing' },
+  { id: 1, col1: 'Pipi', col2: 'er lav', col3: 'test' },
+  { id: 2, col1: 'DataGridPro', col2: 'is Awesome', col3: 'test' },
+  { id: 3, col1: 'Sjokolade', col2: 'is Amazing', col3: 'test'},
+  
 ];
 
 const columns: GridColDef[] = [
   { field: 'col1', headerName: 'Column 1', width: 150 },
   { field: 'col2', headerName: 'Column 2', width: 150 },
+  { field: 'col3', headerName: 'Column 3', width: 150 },
 ];
 
 export default function App() {
